98846a3263c5b8a10617fae42d28a01f63a2f777
Danholder1618 <Danholder2003@yandex.com>
2023-12-20 04:58:39 +0500

    Изменение пароля

--- routers/users.py
+++ routers/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from models.user import User
-from services.user_service import create_user, get_user_by_Username
+from services.user_service import create_user, get_user_by_Username, verify_user_password, change_user_password
 from utils.auth import get_current_user
 from bd import get_db
 
@@ -17,3 +17,28 @@ def create_new_user(user: User, db: Session = Depends(get_db)):
 @router.get("/users/me", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
+
+@router.put("/users/change-password", status_code=status.HTTP_204_NO_CONTENT)
+def change_password(
+    current_password: str,
+    new_password: str,
+    confirm_new_password: str,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+    ):
+
+    if new_password != confirm_new_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="New password and confirmation do not match"
+        )
+
+    if not verify_user_password(current_password, current_user.Password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect current password"
+        )
+
+    change_user_password(db, current_user.UserID, new_password)
+
+    return None
--- services/users_service.py
+++ services/users_service.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from models.user import User
+from security import hash_password
 
 def get_user_by_Username(db: Session, Username: str):
     return db.query(User).filter(User.Username == Username).first()
@@ -10,3 +11,11 @@ def create_user(db: Session, username: str, password: str):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def verify_user_password(plain_password: str, hashed_password: str) -> bool:
+    return hash_password.verify(plain_password, hashed_password)
+
+def change_user_password(db: Session, user_id: int, new_password: str):
+    hashed_password = hash_password(new_password)
+    db.query(User).filter(User.UserID == user_id).update({"Password": hashed_password})
+    db.commit()
