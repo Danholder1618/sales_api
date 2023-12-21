278ae2a2ffe96fb40b24d2aab8810e31355edc9a
Danholder1618 <Danholder2003@yandex.com>
2023-12-21 14:26:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
-from routers import user, product, order
+from routers.users import router as user_router
+from routers.products import router as product_router
+from routers.user_carts import router as cart_router
 
 app = FastAPI()
 
@@ -16,9 +18,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(user.router)
-app.include_router(product.router)
-app.include_router(order.router)
+app.include_router(user_router)
+app.include_router(product_router)
+app.include_router(cart_router)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
