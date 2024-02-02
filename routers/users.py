c5bc62f3f823ab1199cfad01ede721fd5b45b595
Danil <Danholder2003@yandex.com>
2024-02-02 18:24:07 +0500

    Update users.py

--- routers/users.py
+++ routers/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from models.user import User
-from services.users_service import create_user, get_user_by_Username, verify_user_password, change_user_password
+from services.users_service import create_user, get_user_by_username, verify_user_password, change_user_password
 from utils.auth import get_current_user
 from database.db import get_db
 
@@ -9,7 +9,7 @@ router = APIRouter()
 
 @router.post("/users/create_new_user", response_model=User, status_code=status.HTTP_201_CREATED)
 def create_new_user(user: User, db: Session = Depends(get_db)):
-    db_user = get_user_by_Username(db, username=User.Username)
+    db_user = get_user_by_username(db, username=user.Username)
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already registered")
     return create_user(db, user)
