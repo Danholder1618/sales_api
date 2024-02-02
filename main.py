864bcc6970dc028fe1af75ee24afae6ab212a5a5
Danil <Danholder2003@yandex.com>
2024-02-02 18:24:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from routers.users import router as user_router
 from routers.products import router as product_router
 from routers.user_carts import router as cart_router
-from utils.auth import get_current_user
-from database.db import get_db, Session
 
 app = FastAPI()
 
@@ -22,8 +20,6 @@ app.add_middleware(
 app.include_router(user_router)
 app.include_router(product_router)
 app.include_router(cart_router)
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 if __name__ == "__main__":
     import uvicorn
