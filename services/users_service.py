d062da273c24deaf0c2259642ec4f4cf3c692c7f
Danil <Danholder2003@yandex.com>
2024-02-02 18:24:09 +0500

    Update users_service.py

--- services/users_service.py
+++ services/users_service.py
@@ -10,7 +10,7 @@ def get_password_hash(password: str) -> str:
 def verify_user_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_user_by_Username(db: Session, Username: str):
+def get_user_by_username(db: Session, Username: str):
     return db.query(User).filter(User.Username == Username).first()
 
 def create_user(db: Session, username: str, password: str, email: str):
