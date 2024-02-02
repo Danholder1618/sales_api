8bf08524a5426b75d939fd3ceebbfe7adce065ca
Danil <Danholder2003@yandex.com>
2024-02-02 18:24:11 +0500

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,15 +1,17 @@
 from fastapi import Depends, HTTPException, status
-from jose import JWTError, jwt 
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
 from services.users_service import get_user_by_username
 from database.db import get_db
 from models.user import User
-import main
 from sqlalchemy.orm import Session
 from decouple import config
 
 JWT_TOKEN = config('JWT_TOKEN')
 
-def get_current_user(token: str = Depends(main.oauth2_scheme), db: Session = Depends(get_db)) -> User:
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
